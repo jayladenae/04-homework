606832dddc53a55f99e8ae2c6f996a3c5491c431
jayladenae <jaydenaen@icloud.com>
2021-09-27 22:24:21 -0500

    made timer
    added a function for a 5 minute timer that is in seconds

--- 04-homework/assets/javascript/java.js
+++ 04-homework/assets/javascript/java.js
@@ -1,1 +1,17 @@
-//upload
+var startBtn = document.querySelector("#startBtn");
+var timer = document.getElementById('timerDisplay');
+
+
+ startBtn.addEventListener("click", function (){
+   var timeRem = 300; 
+   var timeInt = setInterval(function (){
+       if (timeRem > 1){
+           timer.textContent = timeRem + ' seconds remaining.';
+           timeRem--;
+      } else if (timeRem === 0){
+           timer.textContent = ' ';
+           clearInterval(timeInt);
+       }
+   }, 1000);
+
+   });
