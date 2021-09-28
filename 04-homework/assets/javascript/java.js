d55ae8dde42af0da598edf6daadea203e9afcea0
jayladenae <jaydenaen@icloud.com>
2021-09-27 23:23:56 -0500

    added questions
    added my code questions!

--- 04-homework/assets/javascript/java.js
+++ 04-homework/assets/javascript/java.js
@@ -1,6 +1,58 @@
 var startBtn = document.querySelector("#startBtn");
 var timer = document.getElementById('timerDisplay');
-
+var header = document.getElementById('h1');
+var myQuestions = [
+    {
+        question: "(HTML) How would you write 'Hello World!' as the first header of an HTML doc?",
+        answers: {
+            a: "<h1>Hello World!</h1>",
+            b: "(first-header)Hello World!(end-first-header)",
+            c: "# Hello World!",
+            d: "Hello World!"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "(JavaScript) How do you log to the console?",
+        answers: {
+            a: ".log-to-ocnsole",
+            b: "[save.to.console]",
+            c: "console.log()",
+            d: "#console"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "(JavaScript) How do you declare a variable?",
+        answers: {
+            a: "variable: givenName",
+            b: "var givinName = '_';",
+            c: "$givenName",
+            d: ".declareVariable: '_' "
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "(CSS) How do you change text size using CSS?",
+        answers: {
+            a: "#fontsize: big",
+            b: "50 = text.size",
+            c: "font-size: __px;",
+            d: "//font-size: 5"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "(HTML) Whats the correct syntax for adding a CSS stylesheet to an HTML doc?",
+        answers: {
+            a: "(add.stylesheet)",
+            b: "link: stylesheet;",
+            c: "#stylesheet",
+            d: "<link rel='__' href='__'>"
+        },
+        correctAnswer: "d"
+    }
+];
 
  startBtn.addEventListener("click", function (){
    var timeRem = 300; 
